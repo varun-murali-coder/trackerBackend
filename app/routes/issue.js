bcaf52c9953e09c99a00ede9c1d069ec09b75f44
varun-murali-coder <varunmurali95@gmail.com>
2019-04-01 19:34:45 +0530

    documentation

--- app/routes/issue.js
+++ app/routes/issue.js
@@ -13,14 +13,300 @@ module.exports.setRouter = function(app){
     let baseUrl = `${appConfig.apiVersion}/tickets`;
     
     app.get(baseUrl+'/all',auth.isAuthorized,issueController.getAllTickets);
-    app.get(baseUrl+'/view/:issueId',auth.isAuthorized,issueController.viewByIssueId);
-    app.post(baseUrl+'/create',auth.isAuthorized,issueController.createTicket);
-    app.put(baseUrl+'/:issueId/edit',auth.isAuthorized,issueController.editTicket);
-    app.post(baseUrl+'/comment',auth.isAuthorized,issueController.createComment);
-    app.get(baseUrl+'/comment/:issueId',auth.isAuthorized,issueController.viewCommentByIssueId);
-    app.post(baseUrl+'/search',auth.isAuthorized,issueController.createWatcher);
-    app.get(baseUrl+'/search/:issueId',auth.isAuthorized,issueController.viewWatchersByIssueId);
     
+	/**
+	 * @api {get} /api/v1/tickets/all Get all tickets
+	 * @apiVersion 0.0.1
+	 * @apiGroup read
+	 *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+	 *
+	 *  @apiSuccessExample {json} Success-Response:
+	 *  {
+	    "error": false,
+	    "message": "All Issue Details Found",
+	    "status": 200,
+	    "data": [
+					{
+						issueId: "string",
+						title: "string",
+						assignee: "string",
+						status: "string",
+						reporter: "string",
+						description: "string",
+						created: "date",
+						lastModified: "date"
+					}
+	    		]
+	    	}
+		}
+	}
+	  @apiErrorExample {json} Error-Response:
+	 *
+	 * {
+	    "error": true,
+	    "message": "Failed To Find Blog Details",
+	    "status": 500,
+	    "data": null
+	   }
+	 */
+    app.get(baseUrl+'/view/:issueId',auth.isAuthorized,issueController.viewByIssueId);
+      /**
+	 * @api {get} /api/v1/tickets/view/:issueId Get a single ticket
+	 * @apiVersion 0.0.1
+	 * @apiGroup read
+	 *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+	 * @apiParam {String} issueId The blogId should be passed as the URL parameter
+	 *
+	 *  @apiSuccessExample {json} Success-Response:
+	 *  {
+	    "error": false,
+	    "message": "Issue Found Successfully.",
+	    "status": 200,
+	    "data": {
+	    			_id: "string",
+	    			__v: number
+					issueId: "string",
+						title: "string",
+						assignee: "string",
+						status: "string",
+						reporter: "string",
+						description: "string",
+						created: "date",
+						lastModified: "date"
+				}
+	    	}
+		}
+	}
+	  @apiErrorExample {json} Error-Response:
+	 *
+	 * {
+	    "error": true,
+	    "message": "Error Occured.",
+	    "status": 500,
+	    "data": null
+	   }
+	 */
+    app.post(baseUrl+'/create',auth.isAuthorized,issueController.createTicket);
+     /**
+	 * @api {post} /api/v1/tickets/create Create ticket
+	 * @apiVersion 0.0.1
+	 * @apiGroup create
+	 *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+	 * @apiParam {String} title  of the ticket passed as a body parameter
+	 * @apiParam {String} description  of the ticket passed as a body parameter
+	 * @apiParam {String} assignee  of the ticket passed as a body parameter
+	 * @apiParam {String} reporter  of the ticket passed as a body parameter
+	 *
+	 *  @apiSuccessExample {json} Success-Response:
+	 *  {
+	    "error": false,
+	    "message": "Ticket Created successfully",
+	    "status": 200,
+	    "data": [
+					{
+						issueId: "string",
+						title: "string",
+						assignee: "string",
+						status: "string",
+						reporter: "string",
+						description: "string",
+						created: "date",
+						lastModified: "date"
+					}
+	    		]
+	    	}
+		}
+	}
+	  @apiErrorExample {json} Error-Response:
+	 *
+	 * {
+	    "error": true,
+	    "message": "Error Occured.,
+	    "status": 500,
+	    "data": null
+	   }
+	 */
+    app.put(baseUrl+'/:issueId/edit',auth.isAuthorized,issueController.editTicket);
+
+    /**
+	 * @api {put} /api/v1/ticktes/:issueId/edit Edit ticket by issueId
+	 * @apiVersion 0.0.1
+	 * @apiGroup edit
+	 *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+	 * @apiParam {String} issueId  of the ticket passed as the URL parameter
+	 *
+	 *  @apiSuccessExample {json} Success-Response:
+	 *  {
+	    "error": false,
+	    "message": "Blog Edited Successfully.",
+	    "status": 200,
+	    "data": [
+					{
+						issueId: "string",
+						title: "string",
+						assignee: "string",
+						status: "string",
+						reporter: "string",
+						description: "string",
+						created: "date",
+						lastModified: "date"
+					}
+	    		]
+	    	}
+		}
+	}
+	  @apiErrorExample {json} Error-Response:
+	 *
+	 * {
+	    "error": true,
+	    "message": "Error Occured.,
+	    "status": 500,
+	    "data": null
+	   }
+	 */
+    app.post(baseUrl+'/comment',auth.isAuthorized,issueController.createComment);
+     /**
+	 * @api {post} /api/v1/tickets/comment Create comment
+	 * @apiVersion 0.0.1
+	 * @apiGroup create
+	 *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+	 * @apiParam {String} issueId  of the ticket passed as a body parameter
+	 * @apiParam {String} comment  of the ticket passed as a body parameter
+	 *
+	 *  @apiSuccessExample {json} Success-Response:
+	 *  {
+	    "error": false,
+	    "message": "Comment Created successfully",
+	    "status": 200,
+	    "data": [
+					{
+						issueId: "string",
+						commentId: "string",
+						comment: "string",
+						created: "date",
+						lastModified: "date"
+					}
+	    		]
+	    	}
+		}
+	}
+	  @apiErrorExample {json} Error-Response:
+	 *
+	 * {
+	    "error": true,
+	    "message": "Error Occured.,
+	    "status": 500,
+	    "data": null
+	   }
+	 */
+
+    app.get(baseUrl+'/comment/:issueId',auth.isAuthorized,issueController.viewCommentByIssueId);
+     /**
+	 * @api {get} /api/v1/tickets/comment/:issueId view comment by issueId
+	 * @apiVersion 0.0.1
+	 * @apiGroup read
+	 *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+	 * @apiParam {String} issueId  of the ticket passed as a body parameter
+	 *
+	 *  @apiSuccessExample {json} Success-Response:
+	 *  {
+	    "error": false,
+	    "message": "Comment Pulled successfully",
+	    "status": 200,
+	    "data": [
+					{
+						issueId: "string",
+						commentId: "string",
+						comment: "string",
+						created: "date",
+						lastModified: "date"
+					}
+	    		]
+	    	}
+		}
+	}
+	  @apiErrorExample {json} Error-Response:
+	 *
+	 * {
+	    "error": true,
+	    "message": "Error Occured.,
+	    "status": 500,
+	    "data": null
+	   }
+	 */
+    app.post(baseUrl+'/search',auth.isAuthorized,issueController.createWatcher);
+
+     /**
+	 * @api {post} /api/v1/tickets/search view watchers subscribed to an issue
+	 * @apiVersion 0.0.1
+	 * @apiGroup create
+	 *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+	 * @apiParam {String} issueId  of the ticket passed as a body parameter
+	 *
+	 *  @apiSuccessExample {json} Success-Response:
+	 *  {
+	    "error": false,
+	    "message": "Watcher Created successfully",
+	    "status": 200,
+	    "data": [
+					{
+						issueId: "string",
+						searchId: "string",
+						userName: "string",
+					}
+	    		]
+	    	}
+		}
+	}
+	  @apiErrorExample {json} Error-Response:
+	 *
+	 * {
+	    "error": true,
+	    "message": "Error Occured.,
+	    "status": 500,
+	    "data": null
+	   }
+	 */
+    app.get(baseUrl+'/search/:issueId',auth.isAuthorized,issueController.viewWatchersByIssueId);
+    /**
+	 * @api {get} /api/v1/tickets/search/:issueId view watchers subscribed to an issue
+	 * @apiVersion 0.0.1
+	 * @apiGroup read
+	 *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+	 * @apiParam {String} issueId  of the ticket passed as a body parameter
+	 *
+	 *  @apiSuccessExample {json} Success-Response:
+	 *  {
+	    "error": false,
+	    "message": "Watcher Created successfully",
+	    "status": 200,
+	    "data": [
+					{
+						issueId: "string",
+						searchId: "string",
+						userName: "string",
+					}
+	    		]
+	    	}
+		}
+	}
+	  @apiErrorExample {json} Error-Response:
+	 *
+	 * {
+	    "error": true,
+	    "message": "Error Occured.,
+	    "status": 500,
+	    "data": null
+	   }
+	 */
 
     
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -9,23 +9,24 @@ module.exports.setRouter = (app) => {
     let baseUrl = `${appConfig.apiVersion}/users`;
 
 
-    //app.get(`${baseUrl}/view/all`, auth.isAuthorized, userController.getAllUser);
-
-
-    // params: userId.
-   // app.get(`${baseUrl}/:userId/details`, auth.isAuthorized, userController.getSingleUser);
-
     
-    // params: firstName, lastName, email, mobileNumber, password, apiKey.
     app.post(`${baseUrl}/signup`, userController.signUpFunction);
 
     /**
      * @apiGroup users
      * @apiVersion  1.0.0
-     * @api {post} /api/v1/users/login api for user login.
+     * @api {post} /api/v1/users/signup api for user signup.
      *
-     * @apiParam {string} email email of the user. (body params) (required)
-     * @apiParam {string} password password of the user. (body params) (required)
+     * @apiParam {string} email of the user. (body params) (required)
+     * @apiParam {string} password of the user. (body params) (required)
+     * @apiParam {string} firstName  of the user. (body params) (required)
+     * @apiParam {string} lastName of the user. (body params) (required)
+     * @apiParam {string} mobileNumber  of the user. (body params) (required)
+
+     
+
+
+     * 
      *
      * @apiSuccess {object} myResponse shows error status, message, http status code, result.
      * 
@@ -49,12 +50,56 @@ module.exports.setRouter = (app) => {
 
     app.post(`${baseUrl}/login`, userController.loginFunction);
 
-   // app.put(`${baseUrl}/:userId/edit`, auth.isAuthorized, userController.editUser);
+/**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/users/login api for user login.
+     *
+     * @apiParam {string} email of the user. (body params) (required)
+     * @apiParam {string} password of the user. (body params) (required)
+     *
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+         {
+            "error": false,
+            "message": "Login Successful",
+            "status": 200,
+            "data": {
+                "authToken": "eyJhbGciOiJIUertyuiopojhgfdwertyuVCJ9.MCwiZXhwIjoxNTIwNDI29tIiwibGFzdE5hbWUiE4In19.hAR744xIY9K53JWm1rQ2mc",
+                "userDetails": {
+                "mobileNumber": 2234435524,
+                "email": "someone@mail.com",
+                "lastName": "Sengar",
+                "firstName": "Rishabh",
+                "userId": "-E9zxTYA8"
+            }
 
-   // app.post(`${baseUrl}/:userId/delete`, auth.isAuthorized, userController.deleteUser);
+        }
+    */
 
     
 
     app.post(`${baseUrl}/logout`, auth.isAuthorized, userController.logout);
+    /**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/users/logout api for user logout.
+     *
+	 * @apiParam {String} authToken The token for authentication.(Send authToken as query parameter, body parameter or as a header)
+     * @apiParam {string} userId of the user. (body params) (required)
+     *
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+         {
+            "error": false,
+            "message": "Login Successful",
+            "status": 200,
+            "data": []
+
+        }
+    */
+    
 
 }
